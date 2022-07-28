efd78ef59ec1b41c79cad215c1474dd403a7427d
wissem <wissemb11@gmail.com>
2022-07-28 09:59:20 +0100

    .

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,5 +31,74 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = "sphinx"
+
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+# The master toctree document.
+master_doc = "index"
+copyright = u"2022, Wissem Boughamoura."
+
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = "django-filterdoc"
+
+
+
+# -- Options for manual page output --------------------------------------------
+
+# One entry per manual page. List of tuples
+# (source start file, name, description, authors, manual section).
+man_pages = [
+    (
+        "index",
+        "django-filter",
+        u"django-filter Documentation",
+        [u"Alex Gaynor and others."],
+        1,
+    )
+]
+
+# If true, show URL addresses after external links.
+# man_show_urls = False
+
+
+# -- Options for Texinfo output ------------------------------------------------
+
+# Grouping the document tree into Texinfo files. List of tuples
+# (source start file, target name, title, author,
+#  dir menu entry, description, category)
+texinfo_documents = [
+    (
+        "index",
+        "django-filter",
+        u"django-filter Documentation",
+        u"Alex Gaynor and others.",
+        "django-filter",
+        "One line description of project.",
+        "Miscellaneous",
+    ),
+]
+
+# Documents to append as an appendix to all manuals.
+# texinfo_appendices = []
+
+# If false, no module index is generated.
+# texinfo_domain_indices = True
+
+# How to display URL addresses: 'footnote', 'no', or 'inline'.
+# texinfo_show_urls = 'footnote'
+
+
+# see:
+# https://github.com/snide/sphinx_rtd_theme#using-this-theme-locally-then-building-on-read-the-docs
+on_rtd = os.environ.get("READTHEDOCS", None) == "True"
+
+# only import and set the theme if we're building docs locally
+if not on_rtd:
+    import sphinx_rtd_theme
+
+    html_theme = "sphinx_rtd_theme"
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
